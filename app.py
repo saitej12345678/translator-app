3ecfb5a9e6a33a550cee39b60ad0c51c8defa028
21pa1a6151 Sai Teja <21pa1a6151@vishnu.edu.in>
2025-04-26 16:21:30 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from googletrans import Translator, LANGUAGES
+from flask import Flask, render_template, request
+app = Flask(__name__)
+translator = Translator()
+@app.route('/')
+def home():
+    return render_template('index.html', languages=LANGUAGES)
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    Input_text = request.form['text']
+    dest_lang = request.form['language']
+    translated_text = translator.translate(text=Input_text, dest=dest_lang).text
+    return render_template('index.html', translated_text=translated_text, languages=LANGUAGES)
+if __name__ == '__main__':
+    app.run(debug=True)
